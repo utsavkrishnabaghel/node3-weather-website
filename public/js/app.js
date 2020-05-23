b4fd37a441d748a54b0cbc0261c56ebb286bcee1
utsav <utsavbaghel2406@gmail.com>
2020-05-23 15:09:13 +0530

    remove unneccesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,4 @@
-console.log('client side javascript file is loaded ')
-
-
-
-
-
-
-
-
+//console.log('client side javascript file is loaded ')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
@@ -14,10 +6,9 @@ console.log('client side javascript file is loaded ')
 //     })
 // })
 
-
 //
 //
-//goal: fetch weather 
+//goal: fetch weather
 //
 // 1. setup a call to fetch to fetch weather for new delhi
 // 2. setup the parse json response
@@ -25,46 +16,31 @@ console.log('client side javascript file is loaded ')
 //     - if no error property ,print location and forecast
 // 3. refresh the browser and test your work
 
-
-
-
-
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
 // messageOne.textContent = 'from javascript'
 
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    const location = search.value;
 
+    messageOne.textContent = "Loading....";
+    messageTwo.textContent = "";
 
-
-
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const location = search.value
-
-    messageOne.textContent = 'Loading....'
-    messageTwo.textContent = ''
-
-
-
-
-
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-
-            }
-        })
-    })
-
-
-})
+    fetch("http://localhost:3000/weather?address=" + location).then(
+        (response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error;
+                } else {
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.forecast;
+                }
+            });
+        }
+    );
+});
